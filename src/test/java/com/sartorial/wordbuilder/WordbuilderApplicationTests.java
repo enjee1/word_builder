5221e98cb13f9ebbc177c988a4ad023cbcc15027
Enjee1 <22105805+enjee1@users.noreply.github.com>
2022-03-24 17:54:39 -0400

    Initial commit

---
+++ src/test/java/com/sartorial/wordbuilder/WordbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sartorial.wordbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordbuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
